3663217072b534d123cdeb08b94696be1d532cf6
Rahul Kundal <kundalrahul11@gmail.com>
2024-05-17 03:03:50 -0400

    Full Stack Application Developoment using Spring and React

---
+++ crm/src/main/java/com/crm/crm/model/ContactRepository.java
@@ -0,0 +1,8 @@
+package com.crm.crm.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
